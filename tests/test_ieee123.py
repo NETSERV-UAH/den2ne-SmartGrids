c0c90dd6245ad4b5c80672e4d92ee29cc320fba2
davidcawork <david.carrascal@uah.es>
2025-02-12 16:35:07 +0100

    added debug line to script

--- tests/test_ieee123.py
+++ tests/test_ieee123.py
@@ -18,20 +18,20 @@ class TestIEEE123(unittest.TestCase):
         self.G.pruneGraph()
         self.G_den2ne_alg = Den2ne(self.G)
         
-    def test_spread_ids(self):
+    def test_a_spread_ids(self):
         self.G_den2ne_alg.spread_ids()
         self.assertTrue(len(self.G_den2ne_alg.global_ids) > 0)
 
-    def test_update_loads(self):
+    def test_b_update_loads(self):
         initial_loads = self.G_den2ne_alg.G.nodes["1"].copy()
         self.G_den2ne_alg.updateLoads(self.loads, 1)
         self.assertNotEqual(initial_loads, self.G_den2ne_alg.G.nodes["1"])
 
-    def test_select_best_ids(self):
+    def test_c_select_best_ids(self):
         self.G_den2ne_alg.selectBestIDs(Den2ne.CRITERION_NUM_HOPS)
         self.assertTrue(len(self.G_den2ne_alg.global_ids) > 0)
 
-    def test_global_balance(self):
+    def test_d_global_balance(self):
         self.G_den2ne_alg.updateLoads(self.loads, 1)
         self.G_den2ne_alg.selectBestIDs(Den2ne.CRITERION_NUM_HOPS)
         balance, flux = self.G_den2ne_alg.globalBalance(withLosses=False, withCap=False, withDebugPlot=False, positions=self.positions, path="results/")
