f8cd6e5051c3883e74f581866e24e5a5ef3d01ca
davidcawork <david.carrascal@uah.es>
2025-02-24 15:42:36 +0100

    added exportgraphics matlab scrips

--- src/plot/plotRangeLoads.m
+++ src/plot/plotRangeLoads.m
@@ -4,7 +4,7 @@
 %
 %   [+] Fecha:  14 Feb 2025
 
-function plotRangeLoads_1(init, fin, result_path)
+function plotRangeLoads(init, fin, result_path)
     
     % Preparamos la matriz de datos 
     data3d = zeros(4, 10, fin-init+1); % Se reduce a 4 criterios
@@ -20,58 +20,50 @@ function plotRangeLoads_1(init, fin, result_path)
         data3d(:, :, i-init+1) = data(selected_criteria, :);
     end
     
-    % Obtenemos la media y el error estándar
-    data_avg = mean(data3d(:, 2:7, :), 3);
-    sem = std(data3d(:, 2:7, :), [], 3) / sqrt(fin-init+1);
+    % Obtenemos la media y el error estándar para balance de potencias
+    data_avg = mean(data3d(:, [2 4 6], :), 3);
+    sem = std(data3d(:, [2 4 6], :), [], 3) / sqrt(fin-init+1);
     
-    % Obtenemos la media y el error estándar de los tiempos
-    time_avg = mean(data3d(:, 8:10, :), 3);
-    sem_time = std(data3d(:, 8:10, :), [], 3) / sqrt(fin-init+1);
+    % Pintamos balance global de potencias
+    plotBarWithErrors(data_avg, sem, result_path, 'powerBalance_global.pdf', "Balance de potencias global", "Potencia (kW)");
     
-    % Pintamos la figura de balance global de potencias
-    data_power = data_avg(:, [1 3 5]);
-    sem_power = sem(:, [1 3 5]);
+    % Obtenemos la media y el error estándar para flujo absoluto de potencia
+    data_avg_flux = mean(data3d(:, [3 5 7], :), 3);
+    sem_flux = std(data3d(:, [3 5 7], :), [], 3) / sqrt(fin-init+1);
     
+    % Pintamos flujo absoluto de potencia
+    plotBarWithErrors(data_avg_flux, sem_flux, result_path, 'powerAbsFlux_global.pdf', "Valor absoluto del flujo de potencias", "Potencia (kW)");
+    
+    % Obtenemos la media y el error estándar para tiempos de cálculo
+    time_avg = mean(data3d(:, [8 9 10], :), 3);
+    sem_time = std(data3d(:, [8 9 10], :), [], 3) / sqrt(fin-init+1);
+    
+    % Pintamos tiempos de cálculo
+    plotBarWithErrors(time_avg, sem_time, result_path, 'timestamps_global.pdf', "Tiempos de cálculo", "Tiempo (ms)");
+end
+
+function plotBarWithErrors(data_avg, sem, result_path, filename, title_str, ylabel_str)
     h = figure();
     set(gcf, 'Position', [100 100 900 700]);
-    bar_handle = bar(data_power, 0.6, 'grouped', 'FaceColor', 'flat'); hold on; % Barras más finas
+    bar_handle = bar(data_avg, 0.6, 'grouped', 'FaceColor', 'flat'); hold on;
     
     % Error bars
-    ngroups = size(data_power, 1);
-    nbars = size(data_power, 2);
-    groupwidth = min(0.6, nbars/(nbars + 1.5)); % Espaciado entre barras
+    ngroups = size(data_avg, 1);
+    nbars = size(data_avg, 2);
+    groupwidth = min(0.6, nbars/(nbars + 1.5));
     
     for i = 1:nbars
-        x = bar_handle(i).XEndPoints; % Obtiene las posiciones correctas de las barras
-        errorbar(x, data_power(:, i), sem_power(:, i), 'k', 'linestyle', 'none', 'CapSize', 10);
+        x = bar_handle(i).XEndPoints;
+        errorbar(x, data_avg(:, i), sem(:, i), 'k', 'linestyle', 'none', 'CapSize', 10);
     end
     
     grid on;
-    title("Average global power balance", 'FontSize', 16);
-    ylabel("Power (kW)");
-    legend("Ideal", "Lossy", "Lossy-constrained link capacity", 'Location', 'southoutside', 'NumColumns', 3);
-    set(gca, 'XTickLabel', {'Hops', 'Losses', 'Power 0', 'Power 0 + Losses'});
+    title(title_str, 'FontSize', 16);
+    ylabel(ylabel_str);
+    legend("Ideal", "Con pérdidas", "Con pérdidas y capacidades", 'Location', 'southoutside', 'NumColumns', 3);
+    set(gca, 'XTickLabel', {'Hops', 'Pérdidas', 'Potencia 0', 'Potencia 0 + Pérdidas'});
     hold off;
     
-    print(h, fullfile(result_path, 'fig/powerBalance_global'), '-dpdf', '-r300');
-    
-    % Pintamos los tiempos promedios
-    h = figure();
-    set(gcf, 'Position', [100 100 900 700]);
-    bar_handle = bar(time_avg, 0.6, 'grouped', 'FaceColor', 'flat'); hold on;
-    
-    % Error bars para los tiempos
-    for i = 1:nbars
-        x = bar_handle(i).XEndPoints; % Obtiene las posiciones correctas de las barras
-        errorbar(x, time_avg(:, i), sem_time(:, i), 'k', 'linestyle', 'none', 'CapSize', 10);
-    end
-    
-    grid on;
-    title("Average processing times", 'FontSize', 16);
-    ylabel("Time (ms)");
-    legend("Ideal", "Lossy", "Lossy-constrained link capacity", 'Location', 'southoutside', 'NumColumns', 3);
-    set(gca, 'XTickLabel', {'Hops', 'Losses', 'Power 0', 'Power 0 + Losses'});
-    hold off;
-    
-    print(h, fullfile(result_path, 'fig/processingTimes_global'), '-dpdf', '-r300');
+    % Exportamos la gráfica
+    exportgraphics(h, fullfile(result_path, strcat('fig/', filename)));
 end
