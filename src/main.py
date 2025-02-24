4428a8159a52ce93aa9d82ff9a81987ef6da1fca
davidcawork <david.carrascal@uah.es>
2025-02-24 13:22:35 +0100

    Clear results

--- src/main.py
+++ src/main.py
@@ -80,7 +80,7 @@ def test_ieee123():
     # Recolectamos los datos
     loads = DataGatherer.getLoads("data/loads/loads_v2.csv", 3)
     edges = DataGatherer.getEdges("data/links.csv")
-    edges_conf = DataGatherer.getEdges_Config("data/links/links_config.csv")
+    edges_conf = DataGatherer.getEdges_Config("data/links/links_config_12_5.csv")
     sw_edges = DataGatherer.getSwitches("data/switches.csv")
     positions = DataGatherer.getPositions("data/node_positions.csv")
 
