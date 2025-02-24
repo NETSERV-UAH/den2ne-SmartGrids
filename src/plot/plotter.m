cf8a841a869c4330ad8ecf4b4695583a31f9992e
davidcawork <david.carrascal@uah.es>
2025-02-24 15:41:41 +0100

    rename matlab scrips

--- src/plot/plotter.m
+++ src/plot/plotter.m
@@ -12,10 +12,10 @@ clear varibles
 % Pintamos las graficas correspondientes al balance global de potencias, el
 % valor absoluto del flujo de potencia, perdias por enlaces - switches,
 % perdidas por superar el valor máximo de la configuración de un enlace 
-plotDeltaLoads_1(0,"../results/ieee123/")
+plotDeltaLoads(0,"../results/ieee123/")
 
 % Pintamos el valor medio de todos los intantes de carga 
-plotRangeLoads_1(0, 95, "../results/ieee123/")
+plotRangeLoads(0, 95, "../results/ieee123/")
 
 
 % Pintamos las curvas de los balances
